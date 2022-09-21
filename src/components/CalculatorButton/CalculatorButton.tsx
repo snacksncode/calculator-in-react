cea8bb6beabe7b6300d6e18fb64d9aa21aab33ae
snacksncode <snacksncode@gmail.com>
2022-09-21 23:57:07 +0200

    feat(ui): minor ui changes, testing basic functionality

--- src/components/CalculatorButton/CalculatorButton.tsx
+++ src/components/CalculatorButton/CalculatorButton.tsx
@@ -3,11 +3,12 @@ import { buttonClasses, formatValue } from "./shared";
 interface Props {
   value: string;
   color?: boolean;
+  afterClick: (value: string) => void;
 }
 
-export const CalculatorButton = ({ value, color }: Props) => {
+export const CalculatorButton = ({ value, color, afterClick }: Props) => {
   const handleButtonClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    console.log("handler inside normal button");
+    afterClick(value);
   };
   return (
     <button onClick={handleButtonClick} className={`${color ? "text-blue-300" : "text-slate-900"} ${buttonClasses}`}>
