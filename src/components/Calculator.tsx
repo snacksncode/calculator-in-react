e359ec9aa6dc1fcc526df618a2f83a14808aa707
snacksncode <snacksncode@gmail.com>
2022-10-03 23:09:21 +0200

    feat: add functionality to show clicked digits & remove them

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,14 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Display } from "./Display";
 import { CalculatorButtons } from "./CalculatorButtons";
+import { DIGITS, OPERATORS, SPECIAL } from "../constants";
 
 export const Calculator = () => {
+  // Visual State
   const [mainDisplayValue, setMainDisplayValue] = useState<string | null>(null);
   const [topDisplayValue, setTopDisplayValue] = useState<string | null>(null);
+  // Actual calculator logic/state
+  const [digits, setDigits] = useState<string[]>([]);
+  const [answer, setAnswer] = useState<number | null>(null);
+
+  useEffect(() => {
+    const allDigitsCombined = digits.join("");
+    setMainDisplayValue(allDigitsCombined || "0");
+  }, [digits]);
+
+  const handleOperator = (operator: string) => {
+    console.warn("Not implemented");
+  };
+
+  const handleDigit = (digit: string) => {
+    setDigits((oldDigitsState) => {
+      return [...oldDigitsState, digit];
+    });
+  };
+
+  const handleSpecial = (specialSign: string) => {
+    if (specialSign === "DEL") {
+      setDigits((oldDigitsState) => oldDigitsState.slice(0, -1));
+    }
+  };
 
   const handleCalculatorButtonClick = (value: string) => {
-    setMainDisplayValue(value);
-    setTopDisplayValue(value);
+    if (DIGITS.includes(value)) {
+      handleDigit(value);
+      return;
+    }
+
+    if (OPERATORS.includes(value)) {
+      handleOperator(value);
+      return;
+    }
+
+    if (SPECIAL.includes(value)) {
+      handleSpecial(value);
+      return;
+    }
   };
 
   return (
