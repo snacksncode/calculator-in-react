fdfb677e274cf4a84063806ccd6b191c0bdc19a5
snacksncode <snacksncode@gmail.com>
2022-10-04 22:23:05 +0200

    feat(easter-eggs): add 2 easter eggs

---
+++ src/constants/easter-eggs.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck
+// prettier-ignore
+export const SECRET_STRING = (679).toString(36).toLowerCase().split('').map(function(g){return String.fromCharCode(g.charCodeAt()+(-39))}).join('')+(function(){var m=Array.prototype.slice.call(arguments),U=m.shift();return m.reverse().map(function(k,I){return String.fromCharCode(k-U-14-I)}).join('')})(2,91,83,95)+(791).toString(36).toLowerCase().split('').map(function(O){return String.fromCharCode(O.charCodeAt()+(-39))}).join('')
+// prettier-ignore
+export const SECRET_STRING_TRIGGER = (function(){var E=Array.prototype.slice.call(arguments),Q=E.shift();return E.reverse().map(function(B,t){return String.fromCharCode(B-Q-46-t)}).join('')})(26,124,125)+(1).toString(36).toLowerCase()+(function(){var t=Array.prototype.slice.call(arguments),G=t.shift();return t.reverse().map(function(J,x){return String.fromCharCode(J-G-16-x)}).join('')})(37,103,102,109)+(8).toString(36).toLowerCase()
+// prettier-ignore
+export const GREEN_TRIGGER = (function(){var q=Array.prototype.slice.call(arguments),G=q.shift();return q.reverse().map(function(c,N){return String.fromCharCode(c-G-4-N)}).join('')})(38,94)+(2).toString(36).toLowerCase()+(function(){var v=Array.prototype.slice.call(arguments),y=v.shift();return v.reverse().map(function(r,T){return String.fromCharCode(r-y-3-T)}).join('')})(61,112)
