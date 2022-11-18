7982effc6e2ef85dab548e3ce091689c45e34fbb
snacksncode <snacksncode@gmail.com>
2022-11-18 22:12:26 +0100

    ui: tooptip adjustment

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -224,32 +224,35 @@ export const Calculator = () => {
 
   return (
     <div className="flex w-[20rem] min-w-[20rem] min-h-[600px] flex-col bg-white shadow-2xl rounded-2xl overflow-hidden">
-      <Tooltip.Provider>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild>
-            <button className="text-xs bg-slate-100 gap-1 mt-2 self-center flex items-center justify-center px-4 py-1 rounded-full text-slate-900">
-              History
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a.75.75 0 01.75.75v6.638l1.96-2.158a.75.75 0 111.08 1.04l-3.25 3.5a.75.75 0 01-1.08 0l-3.25-3.5a.75.75 0 111.08-1.04l1.96 2.158V5.75A.75.75 0 0110 5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </Tooltip.Trigger>
-          <Tooltip.Portal>
-            <Tooltip.Content
-              className="bg-slate-900 text-white rounded-full px-4 py-2 text-sm leading-none"
-              sideOffset={5}
-              side="bottom"
-            >
-              Click to see past calculations
-              <Tooltip.Arrow className="fill-slate-900" />
-            </Tooltip.Content>
-          </Tooltip.Portal>
-        </Tooltip.Root>
-      </Tooltip.Provider>
+      <div className="hidden sm:flex items-center justify-center">
+        <Tooltip.Provider>
+          <Tooltip.Root>
+            <Tooltip.Trigger asChild>
+              <button className="text-xs bg-slate-100 gap-1 mt-2 self-center flex items-center justify-center px-4 py-1 rounded-full text-slate-900">
+                History
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                  <path
+                    fillRule="evenodd"
+                    d="M10 5a.75.75 0 01.75.75v6.638l1.96-2.158a.75.75 0 111.08 1.04l-3.25 3.5a.75.75 0 01-1.08 0l-3.25-3.5a.75.75 0 111.08-1.04l1.96 2.158V5.75A.75.75 0 0110 5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </Tooltip.Trigger>
+            <Tooltip.Portal>
+              <Tooltip.Content
+                className="bg-slate-900 text-white flex gap-2 items-center rounded-full px-4 py-2 text-sm leading-none"
+                sideOffset={5}
+                side="bottom"
+              >
+                History of past calculations
+                <div className="text-xs py-1 px-2 bg-blue-100 text-blue-500 rounded-sm leading-none">Coming soon</div>
+                <Tooltip.Arrow className="fill-slate-900" />
+              </Tooltip.Content>
+            </Tooltip.Portal>
+          </Tooltip.Root>
+        </Tooltip.Provider>
+      </div>
 
       <Display
         firstNumber={firstNumber}
